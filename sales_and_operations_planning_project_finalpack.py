4ccf028f03587d65b217883441557837736829e3
Ddembe Galiwango Humphrey <galiwangohumphrey55@gmail.com>
2022-05-31 19:29:45 +0300

    Update sales-and-operations-project
    Edited the structure of the code to meet the format required to be submitted.
    
    Deleted debugging statements from the final program.
    
    Deleted alternative commented code.

--- sales_and_operations_planning_project_finalpack.py
+++ sales_and_operations_planning_project_finalpack.py
@@ -1,9 +1,9 @@
 
 # Ask user to enter the initial stock level, cast to integer and save as initial_stock_level
-initial_stock_level = int(input("Enter the initial stock level: "))
+initial_stock_level = int(input("Please enter the initial stock level: "))
 
 # Ask user to enter the planning horizon (number of months to plan), cast to integer and save as no_of_months
-no_of_months = int(input("Enter the planning horizon: "))
+no_of_months = int(input("Please enter the number of months to plan: "))
 
 # Set an Accumulator for the dictionary of monthly sales
 monthly_sales = {}
@@ -13,22 +13,21 @@ monthly_sales = {}
 for month in range(1, no_of_months+1):
     
     # Ask user to enter the forecasted sale for the month, cast to integer and save as forecast
-    forecast = int(input(f"Enter the forecasted sale for month-{month}: "))
+    forecast = int(input(f"Please enter the planned sales quantity for month-{month}: "))
 
     # Add the forecast to the dictionary of monthly sales
     monthly_sales[month] = forecast
 
-# print(monthly_sales)
+
 
 # Print the resulting production quantities for each month
 
-# Loop through the dictionary of monthly sales:
-# for forecast in monthly_sales.values():  
+# Loop through the dictionary of monthly sales: 
 for month, forecast in monthly_sales.items():  
     
     # Check if forecast_sales > Initial Stock
     if initial_stock_level > forecast:
-        print(f"Production Quantity for Month {month} is: 0")
+        print(f"Production quantity for Month {month} - 0")
         
         # Update the stock level
         initial_stock_level -= forecast
@@ -39,8 +38,7 @@ for month, forecast in monthly_sales.items():
         quantity_produced = forecast - initial_stock_level
         
         # Print the Production Quantity
-        print(f"Production Quantity for Month {month} is: {quantity_produced}")
+        print(f"Production quantity for Month {month} - {quantity_produced}")
         
         # Update the stock level
-        # initial_stock_level = quantity_produced + initial_stock_level - forecast
         initial_stock_level += quantity_produced - forecast
